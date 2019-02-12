25bdcf3400f933c4d1bcc2a9ab61773ee6c9edd7
SalathielGenese <salathielgenese@gmail.com>
2019-02-12 15:02:32 +0100

    FIX import target

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-import '../ts/babylon'
+import * as BABYLON from 'babylonjs';
 
 export class Core {
     private canvas: any;
@@ -7,10 +7,10 @@ export class Core {
 
     constructor() {
         this.canvas = document.getElementById("renderCanvas"); // Get the canvas element
-        this.engine = new BABYLON.Engine(this.canvas, true); // Generate the BABYLON 3D engine 
+        this.engine = new BABYLON.Engine(this.canvas, true); // Generate the BABYLON 3D engine
         this.scene = new BABYLON.Scene(this.engine);
     }
-    
+
     private initScene() {
         // Create the scene space
         this.scene = new BABYLON.Scene(this.engine);;
@@ -26,21 +26,21 @@ export class Core {
 
         return this.scene;
     }
-    
+
     initCamera(): BABYLON.Camera {
         var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(5,20,30), this.scene);
         camera.setTarget(BABYLON.Vector3.Zero());
-        
+
         // camera.mode = BABYLON.Camera.ORTHOGRAPIC_CAMERA;
         // camera.orthoTop = 45;
         // camera.orthoBottom = -45;
         // camera.orthoLeft = -90;
         // camera.orthoRight = 90;
-        
+
         camera.attachControl(this.canvas, true);
         return camera;
     }
-    
+
     initLight (): BABYLON.Light {
         var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 0, 1), this.scene);
         light.intensity = 0.7;
@@ -48,7 +48,7 @@ export class Core {
         // light.specular = new BABYLON.Color3(1,1,0);
         return light;
     }
-    
+
     setMaterial (filename: string, materialName: string): BABYLON.StandardMaterial {
         var material = new BABYLON.StandardMaterial(materialName, this.scene);
         material.diffuseTexture =  new BABYLON.Texture(filename, this.scene);
@@ -57,36 +57,36 @@ export class Core {
         material.ambientTexture =  new BABYLON.Texture(filename, this.scene);
         return material;
     }
-    
+
     createSphere (): BABYLON.Mesh {
         return BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, this.scene);
     }
-    
+
     createGround(position: BABYLON.Vector3): BABYLON.Mesh  {
         var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 200, height: 200}, this.scene);
         ground.position = position;
         ground.material = this.setMaterial('assets/grid.png', "gridMat");
         return ground;
     }
-    
+
     createHolder(position: BABYLON.Vector3, size: number): BABYLON.Mesh {
         // Utils.log("createHolder");
         var holder = BABYLON.MeshBuilder.CreatePlane("holder", { size }, this.scene);
         holder.position = position;
         return holder;
     }
-    
+
     createBox(position: BABYLON.Vector3, height: number, textureFilename: string): BABYLON.Mesh {
         var box = BABYLON.MeshBuilder.CreateBox("boxxy", {height:height, width:1, depth:1}, this.scene);
         box.position = position;
         box.material = this.setMaterial(textureFilename, "boxxyMat");
         return box;
     }
-    
+
     initSpriteManager(name: string, filename: string, width: number, height: number): BABYLON.SpriteManager {
         return new BABYLON.SpriteManager(name, filename, 1, {width: width, height: height}, this.scene);
     }
-    
+
     createSprite(name: string, spriteManager: BABYLON.ISpriteManager): BABYLON.Sprite {
         return new BABYLON.Sprite(name, spriteManager);
     }
